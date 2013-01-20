306bfadb8e40a0c01a1d77a46efd8afd05513839
maibotian <github@meine.us>
2013-01-19 20:36:54 -0500

    changed hover color to blue
    added "Hunger in" to field meta data

--- CountyHungerData.js
+++ CountyHungerData.js
@@ -2,7 +2,7 @@ var countyDataMap_Data ={
 
   "fields":
   [
-    {"name":"county_name","class":"header","description":"County"},
+    {"name":"county_name","class":"header","description":"Hunger in"},
     {"name":"population","class":"primary","description":"Total population"},
     {"name":"food_ins_rate","class":"primary","description":"Food insecurity rate"},
     {"name":"food_ins_rate_child","class":"primary","description":"Child food insecurity rate"},
